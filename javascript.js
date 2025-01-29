806cfa244d5062d018baed400e29f86704ece8d5
Spottedmonkey <186652520+Spottedmonkey@users.noreply.github.com>
2025-01-29 14:46:53 -0600

    Add: gridUpdate button UI

--- javascript.js
+++ javascript.js
@@ -6,15 +6,16 @@ let gridSize = '';
 let totalGridFill = '';
 let gridArea = '';
 
-addGridContent(16, 'red');
+addGridContent(16);
 
 function addGridContent(gridSize, colorSelect) {
     totalGridFill = gridSize * gridSize;
     gridArea = 620/gridSize;
+    colorSelect = setRGBValue();
 
     for (let i = 0; i < totalGridFill; i++) {
         drawingGrid = document.createElement('div');
-        drawingGrid.setAttribute('style', `box-sizing: border-box; border: .1px solid lightgrey; height: ${gridArea}px; width: ${gridArea}px;`);
+        drawingGrid.setAttribute('style', `box-sizing: border-box; border: .1px solid ${colorSelect}; height: ${gridArea}px; width: ${gridArea}px;`);
         drawingGrid.setAttribute('class', 'drawing-pixel');
         drawingContainer.appendChild(drawingGrid);
     }
@@ -38,5 +39,27 @@ gridUpdate.addEventListener('click', () => {
             alert('Grid size can be no smaller than 0')
             gridSize = 1
         }
-    addGridContent(gridSize, "red");
-})
+    addGridContent(gridSize, setRGBValue());
+});
+
+gridUpdate.addEventListener('mouseover', () => {
+    gridUpdate.style.color = '#838483';
+    gridUpdate.style.boxShadow = '-2px -2px 4px #f8faf7, 5px 5px 4px #d8d9d7';
+});
+
+gridUpdate.addEventListener('mouseout', () => {
+    gridUpdate.style.color = '#9e9f9d';
+    gridUpdate.style.boxShadow = '-3px -3px 4px #f8faf7, 3px 3px 4px #d8d9d7';
+});
+
+function setRGBValue(red, green, blue) {
+    red = Math.floor(Math.random() * 255);
+    green = Math.floor(Math.random() * 255);
+    blue = Math.floor(Math.random() * 255);
+
+    let RGBValue = `rgb(${red}, ${green}, ${blue})`
+
+    return RGBValue;
+}
+
+console.log(setRGBValue())
