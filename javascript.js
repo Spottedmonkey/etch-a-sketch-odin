bcf602a3c285c4b590e6f621a4d93862bbc5ddc1
Spottedmonkey <186652520+Spottedmonkey@users.noreply.github.com>
2025-01-29 13:18:48 -0600

    Add: reset grid button

--- javascript.js
+++ javascript.js
@@ -1,59 +1,42 @@
 const drawingContainer = document.querySelector('#drawing-container');
-const gridSizeInput = document.querySelector('#grid-size-input');
-const gridSizeInputValue = document.querySelector('#grid-size-input').value;
+const gridUpdate = document.querySelector('#grid-update');
+const singularDrawingPixel = document.querySelector('.drawing-pixel');
 
-let gridSize = gridSizeInputValue;
-let totalGridFill = gridSize * gridSize;
-let gridArea = 620/gridSize;
+let gridSize = '';
+let totalGridFill = '';
+let gridArea = '';
 
+addGridContent(16, 'red');
 
-function changeGridContent() {
+function addGridContent(gridSize, colorSelect) {
+    totalGridFill = gridSize * gridSize;
+    gridArea = 620/gridSize;
+
     for (let i = 0; i < totalGridFill; i++) {
         drawingGrid = document.createElement('div');
-        drawingGrid.setAttribute('style', `box-sizing: border-box; border: .1px solid lightgrey; height: ${gridArea}px; width: ${gridArea}px;`)
-        drawingGrid.setAttribute('class', 'drawing-pixel')
+        drawingGrid.setAttribute('style', `box-sizing: border-box; border: .1px solid lightgrey; height: ${gridArea}px; width: ${gridArea}px;`);
+        drawingGrid.setAttribute('class', 'drawing-pixel');
         drawingContainer.appendChild(drawingGrid);
     }
+
+    let drawingPixel = document.querySelectorAll('.drawing-pixel');
+
+    drawingPixel.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = `${colorSelect}`;
+        });
+    });
 }
-changeGridContent();
 
-let drawingPixel = document.querySelectorAll('.drawing-pixel');
-
-drawingPixel.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        item.setAttribute('style', `box-sizing: border-box; border: .1px solid lightgrey; height: ${gridArea}px; width: ${gridArea}px;background-color: pink;`)
-    })
+gridUpdate.addEventListener('click', () => {
+    drawingContainer.textContent ='';
+    gridSize = prompt('Please select a new grid size', 16)
+        if (gridSize > 100) {
+            gridSize = 100;
+            alert('Grid size can be no larger than 100');
+        } else if (gridSize < 0) {
+            alert('Grid size can be no smaller than 0')
+            gridSize = 1
+        }
+    addGridContent(gridSize, "red");
 })
-
-console.log(drawingPixel);
-console.log(totalGridFill);
-console.log(gridSizeInputValue);
-
-
-
-
-
-// const drawingContainer = document.querySelector('#drawing-container');
-// let drawingContainerHeight = document.querySelector('#drawing-container').clientHeight;
-// let drawingContainerWidth = document.querySelector('#drawing-container').clientWidth;
-
-// let gridDimensions = 200;
-// let fillContainerHeight = drawingContainerHeight/gridDimensions;
-// let fillContainerWidth = drawingContainerWidth/gridDimensions;
-// let totalContainerFill = fillContainerHeight * fillContainerWidth;
-
-// function changeGridContent() {
-//     for (let i = 0; i < totalContainerFill; i++) {
-//         drawingGrid = document.createElement('div');
-//         drawingGrid.setAttribute('style', `box-sizing: border-box; border: .5px solid lightGrey; height: ${gridDimensions}px; width: ${gridDimensions}px;`);
-//         drawingContainer.appendChild(drawingGrid);
-//     }
-// }
-
-// changeGridContent();
-// console.log(containerSize);
-// console.log(`Container Height: ${drawingContainerHeight}`);
-// console.log(`Container Width: ${drawingContainerWidth}`)
-// console.log(`Grids to fill container height ${fillContainerHeight}`)
-// console.log(`Grids to fill container width ${fillContainerWidth}`)
-// console.log(`Total squares needed to fill the container ${totalContainerFill}`)
