237f4411831986e142884d6baa744dbe8e91a3ee
Spottedmonkey <186652520+Spottedmonkey@users.noreply.github.com>
2025-01-31 17:50:28 -0600

    Add: color opacity increment to drawingPixel

--- javascript.js
+++ javascript.js
@@ -8,7 +8,8 @@ const blackSelect = document.querySelector('#black-select');
 const discoSelect = document.querySelector('#disco-select');
 const brushReset = document.querySelector('#brush-reset');
 const eraserSelect = document.querySelector('#eraser-select');
-const displayDefaultColor = document.querySelector('#display-default-color')
+const displayDefaultColor = document.querySelector('#display-default-color');
+
 
 let redClicked = false;
 let blueClicked = false;
@@ -84,7 +85,7 @@ eraserSelect.addEventListener ('click', () => {
     blueClicked = false;
     redClicked = false;
     blackClicked = false;
-})
+});
 
 function addGridContent(gridSize, colorSelect) {
     totalGridFill = gridSize * gridSize;
@@ -97,24 +98,45 @@ function addGridContent(gridSize, colorSelect) {
         drawingGrid.setAttribute('style', `box-sizing: border-box; border: .1px solid lightgrey; height: ${gridArea}px; width: ${gridArea}px;`);
         drawingGrid.setAttribute('class', 'drawing-pixel');
         drawingContainer.appendChild(drawingGrid);
-    }
-
+    };
+    
     let drawingPixel = document.querySelectorAll('.drawing-pixel');
 
     drawingPixel.forEach((item) => {
         item.addEventListener('mouseover', () => {
+            let currentOpacity = Number(item.style.opacity);
             if (redClicked === true) {
                 item.style.backgroundColor = 'red';
                 item.style.border = '.1px solid red';
+                    if (currentOpacity) {
+                        item.style.opacity = Number(currentOpacity) + 0.1;
+                    } else {
+                        item.style.opacity = 0.1;
+                    };
             } else if (blueClicked === true) {
                 item.style.backgroundColor = 'blue';
                 item.style.border = '.1px solid blue';
+                    if (currentOpacity) {
+                        item.style.opacity = Number(currentOpacity) + 0.1;
+                    } else {
+                        item.style.opacity = 0.1;
+                    };
             } else if (greenClicked === true) {
                 item.style.backgroundColor = 'forestgreen';
                 item.style.border = '.1px solid forestgreen';
+                    if (currentOpacity) {
+                        item.style.opacity = Number(currentOpacity) + 0.1;
+                    } else {
+                        item.style.opacity = 0.1;
+                    };
             } else if (blackClicked === true) {
                 item.style.backgroundColor = 'black';
                 item.style.border = '.1px solid black';
+                    if (currentOpacity) {
+                        item.style.opacity = Number(currentOpacity) + 0.1;
+                    } else {
+                        item.style.opacity = 0.1;
+                    };
             } else if (discoClicked === true) {
                 item.style.backgroundColor = `${setRGBValue()}`;
                 item.style.border = `.1px solid ${setRGBValue()}`;
@@ -124,35 +146,43 @@ function addGridContent(gridSize, colorSelect) {
             } else {
                 item.style.backgroundColor = `${colorSelect}`;
                 item.style.border =`.1px solid ${colorSelect}`;
+                    if (currentOpacity) {
+                        item.style.opacity = Number(currentOpacity) + 0.1;
+                    } else {
+                        item.style.opacity = 0.1;
+                    };
             };
         });
     });
-}
+};
 
 gridUpdate.addEventListener('click', () => {
-    drawingContainer.textContent ='';
     gridSize = prompt('Please select a new grid size', 16)
-        if (gridSize > 100) {
+        if (gridSize === null) {
+            return;
+        } else if (gridSize > 100) {
             gridSize = 100;
             alert('Grid size can be no larger than 100');
         } else if (gridSize < 0) {
             alert('Grid size can be no smaller than 0')
             gridSize = 1
-        }
-    addGridContent(gridSize, setRGBValue());
+        } else {
+            drawingContainer.textContent ='';
+            addGridContent(gridSize, setRGBValue()); 
+        };
 });
 
 gridUpdate.addEventListener('mouseover', () => {
     gridUpdate.style.color = '#838483';
-    gridUpdate.style.boxShadow = '-2px -2px 4px #f8faf7, 5px 5px 4px #d8d9d7';
+    gridUpdate.style.boxShadow = '-2px -2px 4px #f8faf7, 5px 5px 4px #b3b4b2';
 });
 
 gridUpdate.addEventListener('mouseout', () => {
     gridUpdate.style.color = '#9e9f9d';
-    gridUpdate.style.boxShadow = '-3px -3px 4px #f8faf7, 3px 3px 4px #d8d9d7';
+    gridUpdate.style.boxShadow = '-3px -3px 4px #f8faf7, 3px 3px 4px #b3b4b2';
 });
 
-function setRGBValue(red, green, blue) {
+function setRGBValue(red, green, blue,) {
     red = Math.floor(Math.random() * 255);
     green = Math.floor(Math.random() * 255);
     blue = Math.floor(Math.random() * 255);
@@ -160,8 +190,4 @@ function setRGBValue(red, green, blue) {
     let RGBValue = `rgb(${red}, ${green}, ${blue})`
 
     return RGBValue;
-}
-
-console.log(redClicked)
-
-console.log(setRGBValue())
+};
